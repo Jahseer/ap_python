a3dc6dfbb2f55e344851e14d7e3a3d2bcffd6a32
JGriffin <130468109+Jahseer@users.noreply.github.com>
2023-09-21 15:19:53 +0000

    data casting

---
+++ ap_class_activities/catchupwork.py
@@ -0,0 +1,26 @@
+#convert the following datatypes into floats.
+12
+100
+
+12.00
+100.00
+
+# convert the following floats into integers
+12.34
+23.33
+
+1234
+2333
+
+# modify the code below to print an integer number 
+num_one = 20
+num_two = 23.45
+
+print(num_one + num_two)
+print(int(num_one + num_two))
+
+# modify the following the code below to
+name_of_pet = "Spuny"
+age_of_pet = 10
+
+print("this is my dog" + name_of_pet + ".He is" + age_of_pet + " year old.")
