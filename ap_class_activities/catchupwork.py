96f81ab4c1ac7f5f007d03b6620de694e1737929
JGriffin <130468109+Jahseer@users.noreply.github.com>
2023-09-22 14:29:52 +0000

    datacasting

--- ap_class_activities/catchupwork.py
+++ ap_class_activities/catchupwork.py
@@ -1,29 +1,32 @@
-
+# type() function will return the data type of the value
 
 
 #convert the following datatypes into floats.
-12
-100
+a=float(12)
+b=float(100)
 
-12.00
-100.00
+print(a)
+print(b)
+
 
 # convert the following floats into integers
 12.34
 23.33
 
-1234
-2333
+print(int(12.34))
+print(int(23.33))
+
 
 # modify the code below to print an integer number 
 num_one = 20
 num_two = 23.45
 
 print(num_one + num_two)
-print(int(num_one + num_two))
+print(int(23.45))
+print(num_one)
 
 # modify the following the code below to
 name_of_pet = "Spuny"
-age_of_pet = 10
+age_of_pet = str(10)
 
 print("this is my dog" + name_of_pet + ".He is" + age_of_pet + " year old.")
