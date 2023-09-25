6a4b1ebb35f95ef7e5a0e89f1842a463a56ddec0
JGriffin <130468109+Jahseer@users.noreply.github.com>
2023-09-25 02:02:21 +0000

    functionstarter

---
+++ ap_class_activities/functionStarterActivity.py
@@ -0,0 +1,44 @@
+# 1. In your own words, describe what a function is
+
+ ('A function is a block of code which only runs when it is called')
+
+# 2. What is are function parameters and arguments and describe
+# the difference between the 2. 
+
+('A parameter is a variable in a function definition. It is a placeholder and hence does not have a concrete value. An argument is a value passed during function invocation.')
+
+
+
+
+# 3. write a function that will print out a welcome message
+# that includes a users name. You will need to use parameters and arguments
+
+
+def hello(): Jahseer = str(input("Enter your name: ")) if Jahseer: print ("Hello " + str(Jahseer)) else: print("Hello World") return hello()
+
+
+# 4. Write a function that will do a calculation that takes 3 parameters.
+# Your function can do any of the arithmatic operators (add, subtract, multiply, divide)\
+
+a = 4 * 8
+b = 3 + 3
+c = 7 - 3
+print(a)
+print(b)
+print(c)
+
+
+# 5. Write a function that will output a message to a user, telling them
+# what class they have next after this one. this code should use a 
+# variable to pass a value into the parameter of a function. The variable should
+# be real, user data- not hard coded data.
+
+
+A = coding_class
+B = next_class
+
+next_class = reading
+
+print = next_class
+
+ # i don't think any of this is right i just don't know how to do it
---
+++ ap_class_activities/functionlesson.py
@@ -0,0 +1,8 @@
+def welocometoclass():
+    print('welcome to class')
+print('------------------------------')
+
+def greeting(visitor):
+    print('hello' + visitor + 'welcome to python class')
+
+    greeting_msg('Jahseer Griffin')
