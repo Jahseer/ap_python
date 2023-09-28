f29cab8730e41144452ae9af09b71b8be3da7c06
JGriffin <130468109+Jahseer@users.noreply.github.com>
2023-09-28 15:23:43 +0000

    conditionalaACt2

--- ap_class_activities/conditional activity.py
+++ ap_class_activities/conditional activity.py
@@ -1,20 +1,41 @@
 # 1. What is the difference between 
 # a parameter and an argument in a python function
 
-(A parameter is the variable listed inside the parentheses in the function definition. An argument is the value that are sent to the function when it is called.)
-
 # 2. In your own words, describe what 
 # a conditional statement (if/else) is 
 
+# 3. write a simple conditional state using a comparision operator(greater than, less than, etc. )
+
+a = 200
+b = 2
+
+if a > b:
+    print("a is greater than b")
+else:
+    print("b is not greater than a ")
+
+# 4. Write a conditional statement for food in the refridgerator.
 # your conditional statement should be wrapped in a function that takes one (1)
-# parameter. The data type for this parameter should be true or false.  
+# parameter. The data type for this parameter should be true or false. 
+# your function should have a line of code that asks the user if there is food in the refridgerator.
 # If there is food in the refridgerator, print time to cook. If there is 
 # NOT food in the fridge, print time to shop. 
 # When you call your function there should be an argument that accepts 
 # a boolean. 
 
-# 4. Create a function that checks the inventory of cereal for a store. 
-# your function should parameter should accept an integer. In your function
+def foodInFridge(): 
+
+# 5. Create a function that checks the inventory of cereal for a store. 
+# your function should have a parameter that accepts an integer. In your function
 # use a conditional statement to determine if you need to order more cereal.
 # If there is more than 10 boxes, print "inventory full", else if there are less than 
 # 10 boxes print "we need to order more cereal".
+
+def check_cereal_inventory(quantity):
+    if quantity > 18:
+        print('inventory is full.')
+    else:
+        print('order more cereal')
+
+        print('_________________________')
+        
