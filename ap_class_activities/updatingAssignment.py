3ed2bcfb4dd009863348ac1bbaa6973fd4304da2
JGriffin <130468109+Jahseer@users.noreply.github.com>
2023-10-02 14:57:55 +0000

    updatingassignment

---
+++ ap_class_activities/updatingAssignment.py
@@ -0,0 +1,47 @@
+1. # Create a simple list variable that contains 5 list items. It is up to you what will be in your list and what 
+# data type they will be. 
+# Some examples: favorite_atheletes, favorite_games, favorite_books, etc.  
+
+favorite_games = ("call of duty, fortnite, 2k, madden, mortal kombat.")
+print("favorite_games")
+
+
+
+# 2. Find and print the specific item in each list based on their index in the list
+@@ -26,18 +27,22 @@
+# find and print index 0
+random_numbers = [10,100,12123, 1394, 1]
+
+list = random_numbers 
+
+# You have been hired by amazon to be an engineer. Your first assignment is to fix their
+
+# 3. Create a program that will only print out the odd numbers in this list. 
+
+# HINT- part of solving this is that you will need to use the range() function. 
+
+number_list= [1,2,3,4,5,6,7,8,9,10]
+
+Input: list1 = [2, 7, 5, 64, 14]
+Output: [7, 5]
+
+Input: list2 = [12, 14, 95, 3, 73]
+Output: [95, 3, 73]
+
+# 4. You have been hired by amazon to be an engineer. Your first assignment is to fix their
+# shopping cart function. Your goal is to create a line of code that will
+# allow users to enter the item they want as a string value, and add it to the items that
+# are already exist in the cart. Once the new item is entered, a list of all items in the cart should print out. 
+# are already exist in the shopping_cart list variable. 
+# Once the new item is entered, a list of all items in the cart should print out. 
+
+# HINT - for this function you will need to use the append() function. 
+
+shopping_cart = ['notebook', 'pens','tape','mousepad']
+
+
+# 4. Create a program that will only print out the odd numbers in this list. 
+
+# HINT- to solve this you will need to use the range() function. 
+
+number_list= [1,2,3,4,5,6,7,8,9,10]
