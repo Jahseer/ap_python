a344c04a9af91dba1514c54c12bd289c9ada8368
JGriffin <130468109+Jahseer@users.noreply.github.com>
2023-10-26 15:14:44 +0000

    midtermquiz

---
+++ apQuizes/bu/Midtermquiz.py
@@ -0,0 +1,194 @@
+# 1. Describe what a computer program is and what does it do. 
+
+answer = "Its is a set of instructions for a computer to use/perform and it basically does what you instruct it to do"
+
+
+# 2. Describe what complexitity and abstraction are, then provide an example
+# of complexity in the real world and how we apply abstraction to that thing (you example must be original and cannot be an example
+# used in our lecture slides ie car, grocery store, etc.).
+# 
+anwser=("abstraction means to basically put together and complexitiy is to take apart or remove this ")
+
+examples=("Computer, pc")
+
+# 3. What is Syntax in Python and why is it important to write proper syntax?
+
+answer=(" A syntax is a rules that defines how the code will be written and it is important because we wouldn't know how the code would be written if we didn't have this feature ")
+
+# 4. What is a function, and why do we wrap our code inside of functions?
+
+answer=("A function is a block of code that gives the code instructions to eventually call that code but basically short answer a function makes code to run a specific")
+
+# 5. Name and describe the three (3) naming conventions for variables in python? Then provide three (3) name rules for Python
+# variables. 
+
+myName=("camal case")
+
+MyName=("pascal case")
+
+my_name=("Snake case")
+
+answer= ("a varible cannot start with a number,it must start with a letter or underscore and they can only have numeric characters and underscores")
+
+
+
+
+
+
+
+# 6. What will be the output of the print functons when this code is ran, explain why
+name = 'Bill'
+student = name
+name = 'Walter'
+student= 'Richard'
+
+print(name)
+print(student)
+
+answer6= (" the names would print out because you named it name and student this told it to print that and name and student equal to the names so that would print because you instructed it to print the name")
+
+
+# 7. Describe the difference between each of the following assignment operators:
+# = 
+# ==
+# !=
+
+answer7=("we use == to see is the value on the left is equal to the right and != is the see if there not equal and = the to print something that is equal to the code that was instructed")
+
+
+# 8. What type of data can be stored in a python list?
+
+answer8=("int,float")
+
+# 9. Create a function that will take in a word provided by a user and then output that word back to the user but in reverse. 
+
+def reverse():
+    word=("python")
+    if word()
+    print("python")
+   
+
+
+# 10. Create three (3) seperate functions that will do addition, subtraction, and multiplication respectively. 
+# each of these functions should take two (2) arguments. When the user passes these arguments into the function, they will be
+# calculated together and return the output in your terminal. 
+
+def addition():
+    add=('5+5')
+    value=('10')
+
+    print('add')
+
+addition()
+
+
+def subtraction():
+    subtract=('10-5')
+    value=('5')
+
+    print('subtract')
+
+subtraction():
+
+
+def multiplication():
+    multiply=('8*8')
+    value=('64')
+
+    print('multiply')
+
+multiplication():
+
+
+
+
+# 11. What is the difference between a function argument and a function parameter. 
+
+answer11= ('parameters are placeholders and arguments are declared when a function is called')
+
+# 12. You have been hired by a software company and your first assignment is to develop a password authenticator program. 
+# the goal of your program is to check a user's password and make sure it meets the company's password requirements. 
+# the company has provided you with the following requirements for the passcode program:
+# a. the passcode must NOT contain any numbers
+# b. the passcode must NOT contain any capital letters
+# c. the passcode can NOT be any longer than five (5) characters
+# d. the passcode can NOT shorter the three (3) characters
+# e. the user should be able to enter their password and if it meets the requirements, should print a message saying that 
+# their password was created successfully, and if it was not, should send back a message with one of the following issues. 
+
+# 13. When you run your code and see typeError in your terminal, what does that typically mean and how would you try to solve
+# the issue?
+
+answer= ("this means that you might have a used wrong varible or didn't complete the function something wrong or didn't have you code finished")
+
+# 14. When you run your code and see indentError in your terminal, what does that typically mean and how would you try to solve 
+# the issue?
+
+answer14=('when you get this error you didnt indent your code correctly ')
+
+
+# 15. Create a while loop that check a user's password. If they enter the password correct, they will get a message saying 
+# that the password was entered successfully. If they enter the password incorrectly, it will tell the user to try again. 
+# The user should only have three (3) attempts to get the password correct. If they enter the password incorrect on the fourth 
+# attempt, a message should appear telling them that have been locked out and need to talk to the administrator.
+
+def password():
+
+    print('Enter correct username and password combo to continue')
+count=3
+password= ('coding_12')
+
+
+while password!='coding_12' and count<3:
+    password=input('Enter password: ')
+
+    if password=='coding_12' :
+     print('Access granted')
+
+    else:
+        print('Access denied. Try again.')
+        count-=1
+
+
+# 16. Which item is at index 5
+giftShopping=['xbox','sneakers','necklace','clothing','laptop','gift card']
+
+answer=('laptop')
+
+# 17. Create a for loop that will print ONLY the even numbers within the range of the variable provided below.
+# HINT: You will need to use the range() function. 
+
+def numbers():
+    range()
+    numbers=('2,4,6,8,10,12,,14,16,18,20')
+    loop=numbers
+
+    print('numbers')
+
+
+
+
+upToNumber = 30
+
+# 18. Create a function that uses a conditional statement that checks if a person qualifies for a raise on their salary. 
+# The user should be able to enter their name, their salary (how much money they make in an entire year), and how long they have
+# worked at the company. If the user has been working at the company longer than four (4) years, they will get a 15% raise. 
+# Your program should be able to calculate what their salary is with the 15% increase. If the user qualifies, your program
+# should return the a message congratulating the user by name, and telling them what their new salary is with the 15%
+# increase (this must be the actual number). If they do not qualify inform the user that unfortunately they do not qualify. 
+
+# 19. Create a function that checks which value is greater than the other. Your function should take two (2) integer parameters. 
+# and should evaluate which number is larger. Your function should then print the largest number. 
+
+def number():
+    number1=40
+    number2=20
+
+    print("number1 > number2")
+
+number():
+
+# 20. Create a while loop function that will add gift items to a list. Your function should ask the user to enter an item name. 
+# The item name should then be added to a list variable called gift_bag. Your gift bag should have a limit of six (6) items. 
+# Once your gift bag hits its limit of six (6) items your program should print a message saying 
+# that the gift bag is full and print the list of items in the gift bag.   
---
+++ ap_class_activities/forloopswarmup.py
@@ -0,0 +1,15 @@
+# In your own words, describe what a for loop is?
+
+Answer= ('a for loop is used to literat over a python seq')
+
+# 2. What is the difference between a FOR loop and a WHILE loop?
+# Provide two (2) examples of each. 
+
+
+# 3. Create a FOR loop that will go through a list of names 
+# and print all the names that start with the letter "R".
+
+names=['Michael','Rebecca','William','Kareem','Robert','Rose','Jason']#
+for letter in names:
+    if letter[0]=='R':
+        print(letter)
